d99e7f570c22e0358020048275626e6412c784b4
Rodrigo Souza <rsouza01@gmail.com>
2018-12-22 20:25:08 -0200

    First commit

---
+++ src/main/java/com/astrolab/authserver/AstrolabAuthServerApplication.java
@@ -0,0 +1,14 @@
+package com.astrolab.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrolabAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrolabAuthServerApplication.class, args);
+	}
+
+}
+
