7e9ef871fe8aab603f493ff31bf68f07bac7b471
Rodrigo Souza <rsouza01@gmail.com>
2018-12-23 17:41:53 -0200

    Pom.xml fixed

--- src/main/java/com/astrolab/authserver/config/CorsFilterConfiguration.java
+++ src/main/java/com/astrolab/authserver/config/CorsFilterConfiguration.java
@@ -13,15 +13,20 @@ public class CorsFilterConfiguration {
 
     @Bean
     public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+    	UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
+        
         FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        
         return bean;
     }
 
