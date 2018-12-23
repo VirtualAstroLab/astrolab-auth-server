38741a0096d8109fe0fd53dba356a6992a5b5935
Rodrigo Souza <rsouza01@gmail.com>
2018-12-23 01:55:42 -0200

    New controller

--- src/main/java/com/astrolab/authserver/config/WebSecurityConfig.java
+++ src/main/java/com/astrolab/authserver/config/WebSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.astrolab.authserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -16,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
